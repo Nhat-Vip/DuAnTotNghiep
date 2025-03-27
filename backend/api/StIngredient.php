ca0eaff298a426d9e6e51f529ca7ac17a1b6c709
NhatDz <cunhat240907@gmail.com>
2025-03-27 23:44:37 +0700

    update

---
+++ backend/api/StIngredient.php
@@ -0,0 +1,117 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Cho phép gọi API từ frontend
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once "../config/database.php"; // Import kết nối database
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $id = $conn->real_escape_string($_POST["id"]) ?? "";
+    $ingredientId = $conn->real_escape_string($_POST["ingredientId"]) ?? "";
+    $userID = $conn->real_escape_string($_POST["userID"]) ?? "";
+    $subUserID = $conn->real_escape_string($_POST["subUserID"]) ?? "";
+    $name = $conn->real_escape_string($_POST["name"]) ?? "";
+    $quantity = $conn->real_escape_string($_POST["quantity"]) ?? "";
+    $note = $conn->real_escape_string($_POST["note"]);
+}
+
+function check(){
+    global $action, $subUserID, $userID, $quantity,$id,$note;
+
+    if(!isset($quantity)){
+        echo json_encode(["status"=>"Error","message"=>"Không đc để trống số lượng"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+    elseif(!is_float((float)$quantity)){
+        echo json_encode(["status" => "Error", "message" => "Số lượng phải là số"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+    elseif($userID != $subUserID && $action == "update"){
+        echo json_encode(["status"=>"Error","message"=>"Bạn không thể sửa thống kê của người khác"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+    elseif(!isset($id) && $action == "update"){
+        echo json_encode(["status"=>"Error","message"=>"Chưa chọn nguyên liệu để sửa"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+    elseif(!isset($note) || empty($note)){
+        $note = "Empty";
+    }
+}
+
+function GetAll(){
+    global $conn;
+    $todayStart = date("Y-m-d 00:00:00");
+    $todayEnd = date("Y-m-d 23:59:59");
+
+    $sql = "SELECT * FROM coffeeshopmanager.statistical_ingredient
+    where stIngredientDate between '$todayStart' 
+    and '$todayEnd'";
+
+    $result = $conn->query($sql);
+
+    $stIngredient = [];
+
+    if ($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            $stIngredient[] = $row;
+        }
+    }
+
+    echo json_encode($stIngredient,JSON_UNESCAPED_UNICODE);
+}
+
+function Insert(){
+    global $conn,$ingredientId,$userID,$quantity,$note,$name;
+    check();
+    $sql = "INSERT into statistical_ingredient(ingredientId,userID,quantity,ingredientName,note)
+            Values('$ingredientId','$userID','$quantity','$name','$note')";
+    $sql2 = "UPDATE ingredient
+             SET quantity = '$quantity'
+             WHERE ingredientId = '$ingredientId'";
+
+    $conn->begin_transaction();
+    if($conn->query($sql2) == false){
+        $conn->rollback();
+        echo json_encode(["status" => "Error", "Thất bại"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+    if($conn->query($sql) == true){
+        echo json_encode(["status"=>"Success","message"=>"Thành công"], JSON_UNESCAPED_UNICODE);
+    }
+    else{
+        $conn->rollback();
+        echo json_encode(["status"=>"Error","Thất bại"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+
+    $conn->commit();
+}
+
+function Update(){
+    global $conn, $id, $quantity, $note;
+    check();
+    $sql = "UPDATE statistical_ingredient
+            SET quantity = '$quantity',
+                note = '$note'
+            Where stIngredientId = '$id'";
+    if($conn->query($sql) == true){
+        echo json_encode(["status"=>"Success","message"=>"Thành công"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+    else{
+        echo json_encode(["status"=>"Error","message"=>"Thất bại"], JSON_UNESCAPED_UNICODE);
+        exit();
+    }
+}
+$action = $_GET["action"];
+
+if($action == "all"){
+    GetAll();
+}
+elseif($action == "insert"){
+    Insert();
+}
+elseif($action == "update"){
+    Update();
+}
+?>
---
+++ backend/api/product.php
@@ -0,0 +1,169 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Cho phép gọi API từ frontend
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once "../config/database.php"; // Import kết nối database
+include_once "../config/upload.php"; // Import upload ảnh
+
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+
+// Truy vấn danh sách sản phẩm
+function GetAllOfType()
+{
+    global $conn;
+
+    $sql = $sql = "SELECT * FROM product ORDER BY productType";;
+    $result = $conn->query($sql);
+
+    $products = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $type = $row["productType"];
+            $products[$type][] = $row;
+        }
+    }
+
+    // Trả về JSON
+    echo json_encode($products, JSON_UNESCAPED_UNICODE);
+}
+
+function GetAll()
+{
+    global $conn;
+    $sql = "SELECT * FROM product";
+    $result = $conn->query($sql);
+
+    $products = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+    }
+
+
+    // Trả về JSON
+    echo json_encode($products, JSON_UNESCAPED_UNICODE);
+}
+
+function UpdateProduct($id)
+{
+    global $conn;
+
+    if (!isset($_POST["productName"], $_POST["price"], $_POST["type"], $_POST["status"], $_POST["detail"])) {
+        echo json_encode(["status" => "error", "message" => "Thiếu dữ liệu cần cập nhật"]);
+        exit();
+    }
+    // $id = $conn -> real_escape_string(($_POST["id"]));
+
+    // $id = (int)$id;
+    // var_dump($id);
+
+    $name = $conn->real_escape_string($_POST["productName"]);
+    $price = $conn->real_escape_string($_POST["price"]);
+    $type = $conn->real_escape_string($_POST["type"]);
+    $status = $conn->real_escape_string($_POST["status"]);
+    $detail = $conn->real_escape_string($_POST["detail"]);
+
+    $image = isset($_FILES["image"]) ? $_FILES["image"] : null;
+
+    $sql = "UPDATE product 
+                SET productName = '$name', 
+                    price = '$price', 
+                    productStatus = '$status', 
+                    detail = '$detail', 
+                    productType = '$type'";
+
+    if ($image !== null) {
+        $result = upload($image);
+
+        // if ($result) { // Kiểm tra nếu upload thành công
+        //     $image_path = "/Images/" . basename($image["name"]);
+        // }
+        $image_path = "/Images/" . basename($image["name"]);
+        $sql .= ", image_path = '$image_path'";
+    }
+
+    // Thêm điều kiện WHERE
+    $sql .= " WHERE productID = '$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(["status" => "success", "message" => "Cập nhật thành công"]);
+        exit();
+    } else {
+        echo json_encode(["status" => "error", "message" => "Lỗi khi cập nhật: " . $conn->error]);
+        exit();
+    }
+}
+function InsertProduct()
+{
+    global $conn;
+
+    if (!isset($_POST["productName"], $_POST["price"], $_POST["type"], $_POST["status"], $_POST["detail"], $_FILES["image"])) {
+        echo json_encode(["status" => "error", "message" => "Thiếu dữ liệu cần để thêm sản phẩm"]);
+        exit();
+    }
+
+    $name = $conn->real_escape_string($_POST["productName"]);
+    $price = $conn->real_escape_string($_POST["price"]);
+    $type = $conn->real_escape_string($_POST["type"]);
+    $status = $conn->real_escape_string($_POST["status"]);
+    $detail = $conn->real_escape_string($_POST["detail"]);
+
+    $image = isset($_FILES["image"]) ? $_FILES["image"] : null;
+
+    $image_path = "/Images/" . basename($image["name"]);
+
+    $sql = "Insert into product(productName, price, image_path, productStatus, detail, productType)
+                Values ('$name','$price','$image_path','$status','$detail','$type');
+                ";
+    $result = upload($image);
+
+    if ($conn->query($sql) === true) {
+        echo json_encode(["status" => "success", "message" => "Thêm sản phẩm thành công"]);
+        exit();
+    } else {
+        echo json_encode(["status" => "error", "message" => "Lỗi khi thêm: " . $conn->error]);
+        exit();
+    }
+}
+
+function DeleteProduct($id)
+{
+    global $conn;
+    $sql = "Delete from product
+               Where productID = '$id';
+               ";
+    if ($conn->query($sql) === true) {
+        echo json_encode(["status" => "success", "message" => "Xóa sản phẩm thành công"]);
+        exit();
+    } else {
+        echo json_encode(["status" => "error", "message" => "Lỗi khi xóa: " . $conn->error]);
+        exit();
+    }
+}
+
+
+$action = $_GET['action'] ?? ''; // Lấy action từ URL (nếu có)
+// $postAction = $_POST['action'] ?? '';
+$id = $_GET['id'] ?? $_POST['id'] ?? '';
+
+if ($action === "update") {
+    if ($id) {
+        UpdateProduct($id);
+    }
+} elseif ($action == "insert") {
+    InsertProduct();
+} elseif ($action == "delete") {
+    DeleteProduct($id);
+} else if ($action === "all") {
+    GetAll();
+} elseif ($action === "allOfType") {
+    GetAllOfType();
+} else {
+    echo json_encode(["error" => "Invalid action"], JSON_UNESCAPED_UNICODE);
+    exit();
+}
